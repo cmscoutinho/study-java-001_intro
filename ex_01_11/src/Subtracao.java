0c21f722014ee1523255f8627987b65676623c8a
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-17 23:09:05 -0300

    01_11: exercise 05

---
+++ ex_01_11/src/Subtracao.java
@@ -0,0 +1,9 @@
+//5. Crie uma classe Subtração para imprimir o resultado de 10 - 5 no console.
+public class Subtracao {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a-b);
+    }
+}

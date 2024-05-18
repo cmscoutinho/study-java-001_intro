0cb922ecf79d7bce50cf47fdf0e53a554e6be638
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-17 23:07:58 -0300

    01_11: exercise 04

--- ex_01_11/src/Estudo.java
+++ ex_01_11/src/Estudo.java
@@ -1,3 +1,5 @@
+//3. Crie uma classe chamada Estudos para imprimir anotações de estudos no console.
+
 public class Estudo {
     public static void main(String[] args) {
         System.out.println("Java 101: instalar a JVM.");

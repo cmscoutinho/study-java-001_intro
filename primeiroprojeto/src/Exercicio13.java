35b73324da304849c55ff6ce8a2b12ee176df975
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-18 22:05:44 -0300

    02_13: add code for temperature exercise

---
+++ primeiroprojeto/src/Exercicio13.java
@@ -0,0 +1,10 @@
+public class Exercicio13 {
+    public static void main(String[] args) {
+        double tempCelsius = 25.0;
+        double tempFahrenheit;
+
+        tempFahrenheit = (tempCelsius * 1.8) + 32;
+
+        System.out.println("Temperatura em Fahrenheit: " + ((int) tempFahrenheit));
+    }
+}

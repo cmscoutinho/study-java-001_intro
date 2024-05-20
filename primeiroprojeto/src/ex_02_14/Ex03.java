649fe35ab514b52798cb490d3bc1dc0938b84adb
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-19 22:06:07 -0300

    02_14: add code for exercise 3

---
+++ primeiroprojeto/src/ex_02_14/Ex03.java
@@ -0,0 +1,10 @@
+package ex_02_14;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        char varChar = 'C';
+        double varDouble = 10.1;
+
+        System.out.println("Char: %c | Double: %.2f.".formatted(varChar, varDouble));
+    }
+}
